a88795950731bc8c3d3bd6ef2ebd55defec55112
Max Blancarte <blancarte@gmail.com>
2021-02-04 12:36:00 -0800

    updated envio

--- src/pages/enviadoPDF.js
+++ src/pages/enviadoPDF.js
@@ -1,9 +1,15 @@
 import React from "react";
 import BackgroundImage from "gatsby-background-image";
 import { graphql, useStaticQuery } from "gatsby";
-import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import {
+	faFacebookSquare,
+	faFacebookF,
+	faYoutube,
+	faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { faCheckCircle, faStar } from "@fortawesome/free-solid-svg-icons";
+import Img from "gatsby-image";
 import gmailLogo from "../images/gmail.png";
 import chains from "../images/chains_t.png";
 import people from "../images/people_t.png";
@@ -26,12 +32,20 @@ const EnviadoPDF = () => {
 						}
 					}
 				}
+				berenice: file(relativePath: { eq: "berenice.jpg" }) {
+					childImageSharp {
+						fluid(quality: 90, maxWidth: 1080) {
+							...GatsbyImageSharpFluid_withWebp
+						}
+					}
+				}
 			}
 		`
 	);
 
 	const imageData = data.desktop.childImageSharp.fluid;
 	const thumbData = data.thumbs.childImageSharp.fluid;
+	const portrait = data.berenice.childImageSharp.fluid;
 
 	return (
 		<>
@@ -102,7 +116,7 @@ const EnviadoPDF = () => {
 							¿Eres Psicólogo y te gustaría tener mas pacientes?
 						</h1>
 					</div>
-					<div className="flex justify-center py-4 ">
+					<div className="flex justify-center py-10 ">
 						<div className="flex flex-col w-1/2 py-6 text-red-600 bg-white rounded-lg">
 							<p>
 								<span>
@@ -133,19 +147,80 @@ const EnviadoPDF = () => {
 				</div>
 			</BackgroundImage>
 
-			<div className="grid grid-cols-2 bg-red-600">
+			<div className="grid grid-cols-2 py-4 bg-red-600">
 				<div>
 					<h1 className="mt-8 text-3xl font-bold text-center text-white">
 						<h1>Quiero pertenecer a la exclusiva Comunidad Exitosos</h1>
 					</h1>
 				</div>
-				<div className="py-4 my-auto bg-red-600">
+				<div className="my-auto bg-red-600">
 					<div className="flex justify-center py-4">
 						<button className="px-8 py-4 text-xl font-bold text-red-600 transition duration-500 ease-in-out transform bg-white rounded-lg hover:bg-white hover:-translate-y-1 hover:scale-110">
 							<FontAwesomeIcon icon={faFacebookSquare} className="mx-4" />
 							Ir A La Comunidad
 						</button>
 					</div>
+				</div>
+			</div>
+			<div>
+				<div className="grid grid-cols-2">
+					<div className="py-12">
+						<Img fluid={portrait} className="mx-auto rounded-full portrait" />
+					</div>
+					<div>
+						<div className="py-12 text-red-600">
+							<FontAwesomeIcon icon={faStar} className="mx-2" />
+							<FontAwesomeIcon icon={faStar} className="mx-2" />
+							<FontAwesomeIcon icon={faStar} className="mx-2" />
+							<FontAwesomeIcon icon={faStar} className="mx-2" />
+							<FontAwesomeIcon icon={faStar} className="mx-2" />
+							<p className="py-2 ml-2 text-sm text-black">Anfitrión</p>
+							<p className="ml-2 text-lg text-black">
+								Psicóloga Berenice Bastidas
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div>
+				<h1 className="text-lg text-center">Siguenos en redes sociales</h1>
+				<div className="flex flex-row justify-center py-4">
+					<button
+						className="mx-4 text-red-600 bg-red-200 rounded-full outline-none shape hover:bg-red-600 hover:text-white focus:outline-none"
+						onClick={() =>
+							window.open(
+								"https://www.facebook.com/Psic%C3%B3loga-Berenice-Bastidas-365202393876165",
+								"_blank"
+							)
+						}
+					>
+						<FontAwesomeIcon
+							icon={faFacebookF}
+							className="mx-2 text-lg outline-none "
+						/>
+					</button>
+					<button
+						className="mx-4 text-red-600 bg-red-200 rounded-full shape hover:bg-red-600 hover:text-white focus:outline-none"
+						onClick={() =>
+							window.open(
+								"https://www.youtube.com/channel/UCoiQ74yUzLm6ZRUcB_rhPIQ/videos",
+								"_blank"
+							)
+						}
+					>
+						<FontAwesomeIcon icon={faYoutube} className="mx-2 text-lg " />
+					</button>
+					<button
+						className="mx-4 text-red-600 bg-red-200 rounded-full shape hover:bg-red-600 hover:text-white focus:outline-none"
+						onClick={() =>
+							window.open(
+								"http://www.instagram.com/berenicebastidas/?hl=es-la",
+								"_blank"
+							)
+						}
+					>
+						<FontAwesomeIcon icon={faInstagram} className="text-lg " />
+					</button>
 				</div>
 			</div>
 		</>
