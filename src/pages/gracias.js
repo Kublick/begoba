57b634946a23da3b0c2529961f4ce939d322bd5c
max <blancarte@gmail.com>
2021-09-11 23:13:27 -0700

    added new whatsapp url

--- src/pages/gracias.js
+++ src/pages/gracias.js
@@ -59,7 +59,7 @@ const Gracias = () => {
 								className="shadow-lg btn-green logo-container"
 								onClick={() => {
 									window.open(
-										'https://chat.whatsapp.com/Co7XnTlnqsdLCrORdxh1ZG',
+										'https://chat.whatsapp.com/K8Zk5ccESohIDK3GY0jOht',
 									);
 								}}
 							>
